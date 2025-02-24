88e6e08a9734f784598c8ef96682f4f4b0664fb6
Tajetti <talhettialvaro@edu.unifil.br>
2025-02-24 10:27:04 -0300

    update js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ function fecharModal() {
         modal.style.display = 'none';
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.campoLivro');
+    elements.forEach((element) => {
+        element.style.display = 'block'; 
+    });
+});
+
+function filterElements(category, event) {
+    event.preventDefault(); 
+
+    const elements = document.querySelectorAll('.campoLivro');
+
+    elements.forEach((element) => {
+        if (category === "Todos") {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none'; // Oculta todos
+            if (element.classList.contains(category)) {
+                element.style.display = 'block';
+            }
+        }
+    });
+}
